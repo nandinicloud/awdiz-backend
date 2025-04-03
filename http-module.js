c130dee60669f69e4bab7e674db9a6f3883839cc
nandinicloud <163637749+nandinicloud@users.noreply.github.com>
2025-04-03 16:06:09 +0530

    update

---
+++ http-module.js
@@ -0,0 +1,29 @@
+
+const http = require("http");
+// import http from 'http';
+
+// http:fakestoreapi.com/products  [{},{},{}]
+// http:localhost:8000/books books
+
+// frontend -> backend -> request
+// backend -> frontend -> response
+
+const server = http.createServer((req, res) => {
+  console.log("inside server.", req, "req");
+  if (req.method == "GET" && req.url == "/books") {
+    console.log("Inside if.");
+    res.end("My books.");
+  } else if (req.method == "GET" && req.url == "/products") {
+    console.log("Inside if.");
+    res.end("My products.");
+  } else {
+    console.log("Inside else.");
+    res.end("Url not found.");
+  }
+});
+
+const backendPort = 8000;
+
+server.listen(backendPort, () =>
+  console.log(`Server is listening on port ${backendPort}`)
+);
